27e70a176a9597066a58e064747e75e584bb3ae4
jacobimus <jacobo.uribe.bedoya@gmail.com>
2025-02-05 21:31:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,32 @@ let fahrenheit = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
     basic.pause(5000)
-    basic.clearScreen()
+    while (input.temperature() < 8 && input.temperature() > 0) {
+        basic.showNumber(input.temperature())
+        basic.pause(1000)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # . # .
+            . # . # .
+            . # # # .
+            `)
+        basic.pause(3000)
+        basic.clearScreen()
+    }
+    while (input.temperature() < 0) {
+        basic.showNumber(input.temperature())
+        basic.pause(1000)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+        basic.pause(3000)
+        basic.clearScreen()
+    }
 })
 input.onButtonPressed(Button.B, function () {
     fahrenheit = input.temperature() * 9
